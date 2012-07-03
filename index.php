b9ee66e70ed25342c1ff92473da3a13028ec907b
steffenbew <steffen@bleech.de>
2012-07-03 10:15:10 +0200

    moved styles, images & scripts to assets directory + added php/htaccess based cache busting + added less

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="css/main.css">
-    <script src="js/vendor/modernizr-2.5.3.min.js"></script>
+    <link rel="stylesheet" href="assets/styles/css/main.<?php echo filemtime('assets/styles/css/main.css'); ?>.css">
+    <script src="assets/js/vendor/modernizr-2.5.3.min.js"></script>
 </head>
 <body>
 
@@ -24,14 +24,16 @@
 
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="assets/js/plugins.<?php echo filemtime('assets/js/plugins.js'); ?>.js"></script>
+    <script src="assets/js/main.<?php echo filemtime('assets/js/main.js'); ?>.js"></script>
 
-
+    <?php if ( ! isset($_SERVER['ENV']) || $_SERVER['ENV'] !== 'DEVELOPMENT'): ?>
     <script>
         var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
         (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
         g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
         s.parentNode.insertBefore(g,s)}(document,'script'));
     </script>
+    <?php endif; ?>
 </body>
 </html>
